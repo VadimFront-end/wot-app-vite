c3ffa6d6a51619af81e3846ae795174a7c99ada0
VadimFront-end <68503569+VadimFront-end@users.noreply.github.com>
2023-12-20 19:31:34 +0300

    change language for app

--- src/commonForApi.ts
+++ src/commonForApi.ts
@@ -1,6 +1,6 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
 
-export const dataWithAccountId = (data: Record<string, string | number>) => ({ application_id: '38c031f69a335515bf7693436c3a1427', ...data });
+export const dataWithAccountId = (data: Record<string, string | number>) => ({ application_id: '38c031f69a335515bf7693436c3a1427', language: 'ru',...data });
 
 export const baseQuery = fetchBaseQuery({
     baseUrl: 'https://api.worldoftanks.eu/wot/',
